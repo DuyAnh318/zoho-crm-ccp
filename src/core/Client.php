5ef8006342fc5b06cd0150053662f222190a5bdf
DucBT <mdzenos@gmail.com>
2024-06-18 16:31:11 +0700

    update client

--- src/core/Client.php
+++ src/core/Client.php
@@ -1,30 +1,18 @@
 <?php
 
-namespace Zoho;
+namespace Zoho\Core;
 
-/**
- * Container for Zoho record IDs.
- */
 class Client
 {
-    /**
-     * Return a string representation of the list.
-     *
-     * It concatenates the IDs, separated by semicolons.
-     *
-     * @return string
-     */
-
-    private $data;
+    protected $data;
     public function __construct()
     {
+
         $this->data = [
             'name' => 'Zoho API Example',
             'version' => '1.0',
         ];
     }
-
-
     // Hàm để lấy dữ liệu
     public function getData()
     {
