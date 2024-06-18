3e13552fdb8b8af1cb24db86e7a151cf52e6f4c0
DucBT <mdzenos@gmail.com>
2024-06-18 15:07:52 +0700

    Refactor Namespace and Client

---
+++ src/Auth.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zoho;
+
+/**
+ * Container for Zoho record IDs.
+ */
+class Client
+{
+    /**
+     * Return a string representation of the list.
+     *
+     * It concatenates the IDs, separated by semicolons.
+     *
+     * @return string
+     */
+    public function __construct()
+    {
+        return "OK";
+    }
+}
